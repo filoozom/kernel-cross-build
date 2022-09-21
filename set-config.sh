4075102567f072f769fdfbdf2addd2f20343b041
Philippe Schommers <philippe@schommers.be>
2022-09-21 21:59:24 +0200

    feat: add argument to set-config.sh

--- set-config.sh
+++ set-config.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+if [[ $# -ne 1 ]] ; then
+	echo "usage: ./set-config.sh config"
+	exit 1
+fi
+
 . $(pwd)/config.sh
 
 cd $LINUX_FOLDER
 echo "-$PKGREL-$BUILD_FLAVOR" > localversion-alpine
-cp $INITIAL_PWD/kernel-config .config
+cp $INITIAL_PWD/$1 .config
 $INITIAL_PWD/make.sh listnewconfig oldconfig
 
